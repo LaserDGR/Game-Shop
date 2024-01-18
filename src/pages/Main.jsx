6f4a400a833f720fd03b919923935b589955481f
LaserDGR <laserdegante@gmail.com>
2024-01-17 22:31:21 -0600

    second commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -56,7 +56,7 @@ const Main = () => {
   };
 
   const fecthData = () => {
-    fetch("http://localhost:5173/api/gamesData.json")
+    fetch("/api/gamesData.json")
       .then((res) => res.json())
       .then((data) => {
         setGames(data);
